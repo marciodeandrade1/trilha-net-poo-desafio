5b43881ebdad72e699746124a992267a7d1d169c
Marcio de Andrade <gmandrade1@hotmail.com>
2024-01-18 13:53:54 -0400

    desafio POO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone()
+        {
+        }
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+    {
+        // Lógica para instalar o aplicativo no iPhone
+        // Por exemplo:
+        Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+    }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        static void Main()
+        {
+            TestarNokia();
+            TestarIphone();
+            Console.ReadLine();
+        }
+
+        static void TestarNokia()
+        {
+            // Criar uma instância da classe Nokia
+            Nokia nokia = new Nokia();
+
+            // Chamar o método InstalarAplicativo da classe Nokia
+            nokia.InstalarAplicativo("WhatsApp");
+        }
+
+        static void TestarIphone()
+        {
+            // Criar uma instância da classe Iphone
+            Iphone iphone = new Iphone();
+
+            // Chamar o método InstalarAplicativo da classe Iphone
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
